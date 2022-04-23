d06bf3cd51bc7c96a034ab0d12fb3eab7d235f1c
Stefano Zenaro <mariortgasd@hotmail.com>
2022-04-23 21:59:27 +0200

    aggiornata documentazione doxygen

--- docs/doxygen/html/search/all_7.js
+++ docs/doxygen/html/search/all_7.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['inizializzazione_5fmtype',['INIZIALIZZAZIONE_MTYPE',['../defines_8h.html#ac4360beb7ee2bb020a085321d799418f',1,'defines.h']]],
   ['int_5fto_5fstring',['int_to_string',['../client_8c.html#a748983b7ce27515a3ad116ada4334c4e',1,'int_to_string(int value):&#160;client.c'],['../client_8h.html#a748983b7ce27515a3ad116ada4334c4e',1,'int_to_string(int value):&#160;client.c']]]
 ];
